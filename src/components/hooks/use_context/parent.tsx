d8ecb0766969fc2f6ec5d55ae2bc3a4582ca8b70
Your GitHub Usernamemsdubova <femme777@gmail.com>
2023-09-10 15:49:35 +0100

    Use_context

--- src/components/hooks/use_context/parent.tsx
+++ src/components/hooks/use_context/parent.tsx
@@ -1,22 +1,24 @@
-import { useState } from 'react';
-import { Child1 } from './child_1';
-import { Child2 } from './child_2';
+import { useState, createContext } from "react";
+import { Child1 } from "./child_1";
+import { Child2 } from "./child_2";
+
+export const ThemeContext = createContext(true);
 
 export const Parent = () => {
+  const [darkTheme, setDarkTheme] = useState(true);
 
-    const [darkTheme, setDarkTheme] = useState(true)
+  const toggleTheme = () => {
+    setDarkTheme((prevDarkTheme) => !prevDarkTheme);
+  };
 
-    const toggleTheme = () => {
-        setDarkTheme(prevDarkTheme => !prevDarkTheme)
-    }
-
-    return (
-        <div className='section'>
-            <h2>useContext</h2>
-            <button onClick={toggleTheme}>Toggle Theme</button>
-            <Child1 dark={darkTheme} />
-            <Child2 dark={darkTheme} />
-        </div>
-
-    )
-}
+  return (
+    <div className="section">
+      <h2>useContext</h2>
+      <button onClick={toggleTheme}>Toggle Theme</button>
+      <ThemeContext.Provider value={darkTheme}>
+        <Child1 />
+        <Child2 />
+      </ThemeContext.Provider>
+    </div>
+  );
+};
